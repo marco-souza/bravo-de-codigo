328a5477a2333e25687c9d98082685f45783ff8e
Marco Antônio <ma.souza.junior@gmail.com>
2019-04-24 14:31:32 -0300

    ENH: add script to validade load of login screen

---
+++ puppeteer-test/index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer')
+
+const main = async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ]
+    })
+
+    const page = await browser.newPage()
+    await page.goto('https://app.smarttbot.com', { waitUntil: 'networkidle0' })
+
+    // check if #login-username is present
+    console.assert(!page.$("#login-username"))
+    // check if #login-password is present
+    console.assert(page.$("#login-password"))
+    // check if #login-busson is present
+    console.assert(page.$("#login-button"))
+    await page.screenshot({ path: 'example.png' })
+
+    // Close connection
+    await browser.close()
+}
+
+main()
